321b431d5340666842248cf8cd907bbae2d1f83f
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2022-04-18 09:52:25 +0200

    different call

--- java_design_pattern/src/at/ac/htlinn/design_pattern/strategy/payment/ShoppingCartRunner.java
+++ java_design_pattern/src/at/ac/htlinn/design_pattern/strategy/payment/ShoppingCartRunner.java
@@ -3,18 +3,18 @@ package at.ac.htlinn.design_pattern.strategy.payment;
 public class ShoppingCartRunner {
 
 	public static void main(String[] args) {
-ShoppingCart cart = new ShoppingCart();
-		
-		Article item1 = new Article("1", "Raspberry Pi 4",10);
-		Article item2 = new Article("2", "Java ist auch eine Insel",40);
-		
+		ShoppingCart cart = new ShoppingCart();
+
+		Article item1 = new Article("1", "Raspberry Pi 4", 10);
+		Article item2 = new Article("2", "Java ist auch eine Insel", 40);
+
 		cart.addItem(item1);
 		cart.addItem(item2);
-		
-		//pay by paypal
+
+		// pay by paypal
 		cart.pay(new PaypalStrategy("user1@gmail.com", "mypwd"));
-		
-		//pay by credit card
+
+		// pay by credit card
 		cart.pay(new CreditCardStrategy("Max Mustermann", "1234567890123456", "445", "12/23"));
 
 	}
