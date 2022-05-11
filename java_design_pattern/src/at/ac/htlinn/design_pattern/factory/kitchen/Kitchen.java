8d58d1d7191e718aaa5631e01333c4a54d5ad519
albert.greinoecker@gmail.com <albert.greinoecker@gmail.com>
2022-05-11 08:54:23 +0200

    translate

--- java_design_pattern/src/at/ac/htlinn/design_pattern/factory/kitchen/Kitchen.java
+++ java_design_pattern/src/at/ac/htlinn/design_pattern/factory/kitchen/Kitchen.java
@@ -2,7 +2,7 @@ package at.ac.htlinn.design_pattern.factory.kitchen;
 
 public class Kitchen {
 	public static void main(String[] args) {
-		Meal s1 = MealFactory.erzeugen("Lasagne");
+		Meal s1 = MealFactory.prepare("Lasagne");
 		System.out.println(s1.getClass().getName());
 
 	}
